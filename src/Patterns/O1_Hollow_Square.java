df35af6c775757e7abf53f1c708c884a5f1d4864
AftabJ09 <aftab.50100@gmail.com>
2025-10-13 12:59:08 +0530

    Hollow Square Pattern

---
+++ src/Patterns/O1_Hollow_Square.java
@@ -0,0 +1,15 @@
+package Patterns;
+public class O1_Hollow_Square {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                if(i==0 || j ==0 || i == 9 || j==9){
+                    System.out.print("*  ");
+                }else{
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
